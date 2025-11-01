7d95f5ef0cc48f3964cf8bd7a21cf5cca8c177ed
Danya <dansteamer1337@gmail.com>
2025-11-01 12:17:12 +0300

    Добавьте файлы проекта.

---
+++ WebApplication4/Controllers/TestController.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+[ApiController]
+[Route("[controller]")]
+public class TestController : ControllerBase
+{
+    private static readonly Random random = new Random();
+
+    [HttpGet]
+    public int[] GetRandomNumbers(int count)
+    {
+        if (count <= 0)
+        {
+            return Array.Empty<int>();
+        }
+
+        var numbers = new int[count];
+        for (int i = 0; i < count; i++)
+        {
+            numbers[i] = random.Next(1, 101);
+        }
+
+        return numbers;
+    }
+}
